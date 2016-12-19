f4ea24d9f3e96d35a6b53c99a184cd5517ad5e56
Dave Harms <daveharmswebdev@gmail.com>
2016-12-18 22:47:41 -0600

    added post and get routes for mongo

---
+++ app/routes/users.server.routes.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const users = require('../../app/controllers/users.server.controller')
+
+module.exports = app => {
+	app.route('/users')
+		.post(users.create)
+		.get(users.list)
+}
